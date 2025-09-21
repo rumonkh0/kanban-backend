ea967984a4c696df38a88c0a6a7882a326db1282
rumonkh0 <rummankh0@gmail.com>
2025-09-21 12:39:24 +0600

    connection setup

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const errorHandler = require('./middleware/error');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 // Connect to database
-// connectDB();
+connectDB();
 
 // Route files
 
@@ -31,16 +31,11 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // File uploading
-app.use(fileupload());
 
-// Sanitize data
-app.use(mongoSanitize());
 
 // Set security headers
 app.use(helmet());
 
-// Prevent XSS attacks
-app.use(xss());
 
 // Rate limiting
 const limiter = rateLimit({
@@ -64,13 +59,13 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 );
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`.red);
+  console.log(`Error: ${err.message}`);
   // Close server & exit process
   // server.close(() => process.exit(1));
 });
